7eaee3c0ed95e95a289be91459acfd6414a23803
Elon Chiu <elon.chiu@tpinformation.com.tw>
2017-12-18 18:23:25 +0800

    part5

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   styles: [`
@@ -65,27 +66,25 @@ import { Hero } from './hero';
       </li>
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
-    `
+    `,
+    providers: [HeroService]
 })
-export class AppComponent {
-  heroes = HEROES
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
   
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
