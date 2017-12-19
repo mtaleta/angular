5222cf3b09f7155ed2037e2686cdd03f46304f0b
Elon Chiu <elon.chiu@tpinformation.com.tw>
2017-12-19 17:00:44 +0800

    part 5

--- my-app/src/app/hero-detail.component.ts
+++ my-app/src/app/hero-detail.component.ts
@@ -1,21 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from './hero.service';
+
+// 告訴這個類，我們要實現OnInit接口。
+import 'rxjs/add/operator/switchMap';
 import { Hero } from './hero';
+// import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
-  template:`
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div><label>name: </label>[{{hero.name}}]</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html',
+  styleUrls: [ './hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
+
+export class HeroDetailComponent implements OnInit{
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+  
+  goBack(): void {
+    this.location.back();
+  }
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => 
+        this.heroService.getHero(+params.get('id')))
+      .subscribe(hero => this.hero = hero);
+  }
   @Input() hero: Hero;
 }
